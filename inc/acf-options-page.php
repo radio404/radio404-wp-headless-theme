7a6f20ac4077260b5ea3ae237a28a76376d720ce
Arthur Violy <arthur@violy.net>
2019-11-15 11:42:59 +0100

    ACF options page 'radio'

--- inc/acf-options-page.php
+++ inc/acf-options-page.php
@@ -9,6 +9,7 @@ if( function_exists('acf_add_options_page') ) {
 		'menu_title'	=> $page_title,
 		'menu_slug'     => $page_slug,
 		'slug'          => $page_slug,
+		'post_id'       => $page_slug,
 		'icon_url' => 'dashicons-admin-tools',
 	]);
 
